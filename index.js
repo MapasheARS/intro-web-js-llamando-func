0b76e3cdd4ef5c8ae8f3eb89701fcb70f36609f4
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-28 11:37:04 -0500

    Actualizacion, llamando a una funcion

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // declara una variable contador con valor 5
-
-
+let contador = 5
 // incrementarContador() incrementa el contador por 1
-
-
+function incrementaContador() {
+    contador++
+}
 // decrementarContador() decrementa el contador por 1
-
-
+function decrementarContador(){
+    contador--
+}
 // imprimirContador() imprime el contador en la consola
-
-
+function imprimirContador(){
+    console.log(contador)
+}
 // Llama tres veces a incrementarContador()
-
-
+incrementaContador()
+incrementaContador()
+incrementaContador()
 // Llama una vez a decrementarContador()
-
-
+decrementarContador()
 // Llama una vez a imprimirContador()
+imprimirContador()
